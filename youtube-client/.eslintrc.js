b109023bb47535be26c4de9563bb8aa4e856e4d4
MaxKoval1ov <maxkov112211@gmail.com>
2022-03-25 21:46:32 +0300

    feat: fix linter

---
+++ youtube-client/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin", "import"],
+  extends: ["plugin:@typescript-eslint/recommended", "airbnb-typescript/base"],
+  root: true,
+  ignorePatterns: [".eslintrc.js", "*.html"],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
--- youtube-client/src/app/search/sort/sort.component.spec.ts
+++ youtube-client/src/app/search/sort/sort.component.spec.ts
@@ -8,9 +8,8 @@ describe('SortComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SortComponent ]
-    })
-    .compileComponents();
+      declarations: [SortComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
